0bf17e74c5b369d3cb3c81f73f5db7072bf66ba3
Eugene Korobkov <e@korobkov.io>
2021-10-28 09:48:54 +0300

    feat: index page added loading on page change

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,10 @@
-<p><a href="/existing">Existing post</a></p>
-<p><a href="/non-existent">non-existent post</a></p>
+<script>
+	import { navigating } from '$app/stores';
+</script>
+
+{#if $navigating}
+	Loading...
+{:else}
+	<p><a href="/existing">Existing post</a></p>
+	<p><a href="/non-existent">non-existent post</a></p>
+{/if}
