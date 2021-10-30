06409d4957d50f63ce5581d43ee7e5efef88aea1
Korobkov Mikhail <m@korobkov.io>
2021-10-30 19:23:37 +0300

    fix: new way to import @tsndr/cloudflare-worker-jwt package

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,13 +2,13 @@ import { parse } from 'cookie';
 import { prerendering } from '$app/env';
 import type { Locals } from '$lib/types';
 import type { Handle } from '@sveltejs/kit';
-import jwt from '@tsndr/cloudflare-worker-jwt';
 import type { User } from './routes/users/_typings';
 
 const AUTH_COOKIE_NAME = 'token';
 
 export const handle: Handle<Locals> = async ({ request, resolve }) => {
   if (!prerendering) {
+    const jwt = (await import('@tsndr/cloudflare-worker-jwt')).default;
     const cookie = parse(request.headers['cookie'] || '');
     const signedToken = cookie[AUTH_COOKIE_NAME];
     if (signedToken) {
