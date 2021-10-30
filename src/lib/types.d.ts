b0702ae1f8dc2a4ddc901bfc0573acd8fc82e858
Korobkov Mikhail <m@korobkov.io>
2021-10-30 18:43:39 +0300

    refactor: move User type to users dir

--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -1,13 +1,11 @@
+import { User } from '../routes/users/_typings';
+
 /**
  * Can be made globally available by placing this
  * inside `global.d.ts` and removing `export` keyword
  */
 export interface Locals {
 	user?: User;
-}
-
-export interface User {
-	id: string;
 }
 
 // https://github.com/sveltejs/kit/issues/1997
