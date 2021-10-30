4324046892c92fbecb0f6f03b1653715821e8e4a
Eugene Korobkov <e@korobkov.io>
2021-10-30 12:04:32 +0300

    feat(document form): label styling

--- src/lib/document-form.svelte
+++ src/lib/document-form.svelte
@@ -29,13 +29,13 @@
 </script>
 
 <form on:submit|preventDefault={submitForm}>
-	<label>
+	<label class:has-value={formTitle.length > 0}>
+		<input bind:value={formTitle} class="input title" />
 		<span>Title</span>
-		<input bind:value={formTitle} class="input title" placeholder="Title" />
 	</label>
-	<label>
+	<label class:has-value={formAuthor.length > 0}>
+		<input bind:value={formAuthor} class="input author" />
 		<span>Author</span>
-		<input bind:value={formAuthor} class="input author" placeholder="Author" />
 	</label>
 	<TipTap bind:content={formContent} editable={true} />
 </form>
@@ -46,16 +46,41 @@
 		flex-direction: column;
 	}
 
+	label {
+		position: relative;
+
+		&.has-value > span {
+			visibility: hidden;
+			right: 100%;
+			left: auto;
+			opacity: 0;
+		}
+
+		&.has-value {
+			input:focus + span {
+				visibility: visible;
+				opacity: 1;
+			}
+		}
+
+		& > span {
+			touch-action: none;
+			color: rgba(0, 0, 0, 0.4);
+			position: absolute;
+			top: 50%;
+			left: 0;
+			transform: translateY(-50%);
+			transition: opacity 0.3s;
+		}
+	}
+
 	.input {
 		border: none;
 		outline: none;
 		font-family: Roboto, sans-serif;
 		font-style: normal;
 		font-weight: normal;
-		color: rgba(0, 0, 0, 0.4);
-		&::placeholder {
-			color: red;
-		}
+		color: rgba(0, 0, 0, 0.8);
 	}
 
 	.title {
