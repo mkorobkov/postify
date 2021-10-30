89e5d49d5e69382c83439127b36055580369c149
Korobkov Mikhail <m@korobkov.io>
2021-10-30 18:44:51 +0300

    feat: decode user from token cookie(jwt) on each request

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,8 +1,23 @@
-import type { Handle } from '@sveltejs/kit';
+import { parse } from 'cookie';
+import { prerendering } from '$app/env';
 import type { Locals } from '$lib/types';
+import type { Handle } from '@sveltejs/kit';
+import jwt from '@tsndr/cloudflare-worker-jwt';
+import type { User } from './routes/users/_typings';
+
+const AUTH_COOKIE_NAME = 'token';
 
 export const handle: Handle<Locals> = async ({ request, resolve }) => {
-	request.locals.userid = 'user id passed';
+  if (!prerendering) {
+    const cookie = parse(request.headers['cookie'] || '');
+    const signedToken = cookie[AUTH_COOKIE_NAME];
+    if (signedToken) {
+      const isValid = await jwt.verify(signedToken, global.JWT_SECRET);
+      if (isValid) {
+        request.locals.user = jwt.decode(signedToken) as User;
+      }
+    }
+  }
 
-	return resolve(request);
+  return resolve(request);
 };
