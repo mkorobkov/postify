ef645cb25a2c665163a9636a6fe5952de9e5de01
Korobkov Mikhail <m@korobkov.io>
2021-10-30 20:49:06 +0300

    feat: generate new user if got request without auth

--- src/hooks.ts
+++ src/hooks.ts
@@ -3,21 +3,37 @@ import { prerendering } from '$app/env';
 import type { Locals } from '$lib/types';
 import type { Handle } from '@sveltejs/kit';
 import type { User } from './routes/users/_typings';
+import { v4 as uuid } from '@lukeed/uuid';
+import { generateAuthToken } from './routes/users/_utils';
 
 const AUTH_COOKIE_NAME = 'token';
 
 export const handle: Handle<Locals> = async ({ request, resolve }) => {
-  if (!prerendering) {
-    const jwt = (await import('@tsndr/cloudflare-worker-jwt')).default;
-    const cookie = parse(request.headers['cookie'] || '');
-    const signedToken = cookie[AUTH_COOKIE_NAME];
-    if (signedToken) {
-      const isValid = await jwt.verify(signedToken, global.JWT_SECRET);
-      if (isValid) {
-        request.locals.user = jwt.decode(signedToken) as User;
-      }
+  if (prerendering) return resolve(request);
+  const jwt = (await import('@tsndr/cloudflare-worker-jwt')).default;
+  const cookie = parse(request.headers['cookie'] || '');
+  const signedToken = cookie[AUTH_COOKIE_NAME];
+  if (signedToken) {
+    const isValid = await jwt.verify(signedToken, global.JWT_SECRET);
+    if (isValid) {
+      request.locals.user = jwt.decode(signedToken) as User;
     }
   }
 
-  return resolve(request);
+  if (request.locals.user) return resolve(request);
+
+  /* generate new user and assign to current request */
+  const newUser: User = {
+    id: uuid(),
+    name: 'Anonymous',
+  };
+
+  request.locals.user = newUser;
+
+  const response = await resolve(request);
+
+  /* add auth token for future requests */
+  response.headers['Set-Cookie'] = `token=${await generateAuthToken(newUser)};HttpOnly`;
+
+  return response;
 };
