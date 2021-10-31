b801f442394c41af7dce1a2635d278da7b3c3dff
Korobkov Mikhail <m@korobkov.io>
2021-10-31 10:11:03 +0300

    refactor: use typings on GET users/me

--- src/routes/users/me.ts
+++ src/routes/users/me.ts
@@ -1,11 +1,10 @@
-import type { Locals } from '$lib/types';
+import type { Locals, Typify } from '$lib/types';
 import type { RequestHandler } from '@sveltejs/kit';
-import type { JSONString } from '@sveltejs/kit/types/helper';
+import type { GetUsersMeResponse } from './_typings';
 
-
-export const get: RequestHandler<Locals> = async (request) => {
-  return {
-    status: 200,
-    body: { success: true, user: request.locals.user as any as JSONString },
-  };
+export const get: RequestHandler<Locals, unknown, Typify<GetUsersMeResponse>> = async (request) => {
+	return {
+		status: 200,
+		body: { success: true, user: request.locals.user },
+	};
 };
