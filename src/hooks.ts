4cba46d71ca606ad87fe95154ac41b24676de331
Eugene Korobkov <e@korobkov.io>
2021-10-31 09:54:11 +0300

    fix: if dev, then return to work with svelte dev

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,39 +1,41 @@
+import { dev, prerendering } from '$app/env';
 import { parse } from 'cookie';
-import { prerendering } from '$app/env';
+
+import { v4 as uuid } from '@lukeed/uuid';
+
+import { generateAuthToken } from './routes/users/_utils';
+
 import type { Locals } from '$lib/types';
 import type { Handle } from '@sveltejs/kit';
 import type { User } from './routes/users/_typings';
-import { v4 as uuid } from '@lukeed/uuid';
-import { generateAuthToken } from './routes/users/_utils';
-
 const AUTH_COOKIE_NAME = 'token';
 
 export const handle: Handle<Locals> = async ({ request, resolve }) => {
-  if (prerendering) return resolve(request);
-  const jwt = (await import('@tsndr/cloudflare-worker-jwt')).default;
-  const cookie = parse(request.headers['cookie'] || '');
-  const signedToken = cookie[AUTH_COOKIE_NAME];
-  if (signedToken) {
-    const isValid = await jwt.verify(signedToken, JWT_SECRET);
-    if (isValid) {
-      request.locals.user = jwt.decode(signedToken) as User;
-    }
-  }
+	if (dev || prerendering) return resolve(request);
+	const jwt = (await import('@tsndr/cloudflare-worker-jwt')).default;
+	const cookie = parse(request.headers['cookie'] || '');
+	const signedToken = cookie[AUTH_COOKIE_NAME];
+	if (signedToken) {
+		const isValid = await jwt.verify(signedToken, JWT_SECRET);
+		if (isValid) {
+			request.locals.user = jwt.decode(signedToken) as User;
+		}
+	}
 
-  if (request.locals.user) return resolve(request);
+	if (request.locals.user) return resolve(request);
 
-  /* generate new user and assign to current request */
-  const newUser: User = {
-    id: uuid(),
-    name: '',
-  };
+	/* generate new user and assign to current request */
+	const newUser: User = {
+		id: uuid(),
+		name: ''
+	};
 
-  request.locals.user = newUser;
+	request.locals.user = newUser;
 
-  const response = await resolve(request);
+	const response = await resolve(request);
 
-  /* add auth token for future requests */
-  response.headers['Set-Cookie'] = `token=${await generateAuthToken(newUser)};HttpOnly`;
+	/* add auth token for future requests */
+	response.headers['Set-Cookie'] = `token=${await generateAuthToken(newUser)};HttpOnly`;
 
-  return response;
+	return response;
 };
