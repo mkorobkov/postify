918e91be925d396af1279a41e3be8eec609a56db
Korobkov Mikhail <m@korobkov.io>
2021-10-31 19:40:43 +0300

    feat: redirect from /docs to /

---
+++ src/routes/docs/index.svelte
@@ -0,0 +1,7 @@
+<script context="module" lang="ts">
+	import type { Load } from '@sveltejs/kit';
+
+	export const load: Load = async () => {
+		return { status: 301, redirect: '/' };
+	};
+</script>
