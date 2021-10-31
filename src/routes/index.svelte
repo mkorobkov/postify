e973eebe36fb733760af5b8c38b5ce8ecfc3e852
Eugene Korobkov <e@korobkov.io>
2021-10-31 17:21:57 +0300

    feat: del create page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,62 @@
-<script>
-	import { navigating } from '$app/stores';
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import DocumentForm, { FormDocument } from '$lib/document-form.svelte';
+	import type { SvelteComponentTyped } from 'svelte';
 	import Layout from '$lib/layout.svelte';
+	import Button from '$lib/button.svelte';
+	import type { PostDocumentInput, PostDocumentResponse } from './docs/_typings';
+
+	let documentFormRef: (SvelteComponentTyped & { submitForm(): unknown }) | undefined;
+	let loading = false;
+
+	async function createDocument(data: FormDocument) {
+		const body: PostDocumentInput = {
+			...data,
+			isEncrypted: false,
+		};
+
+		const res = await fetch('/docs', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(body),
+		});
+		const result = await res.json<PostDocumentResponse>();
+
+		if (result.success === false) {
+			throw new Error(result?.message);
+		}
+
+		return result.data;
+	}
+
+	async function handleSubmit(event: CustomEvent<FormDocument>) {
+		loading = true;
+
+		try {
+			const result = await createDocument(event.detail);
+			goto(`/${result.document.documentId}`);
+		} catch (err) {
+			alert(err?.message ?? 'Error occurred');
+		} finally {
+			loading = false;
+		}
+	}
 </script>
 
 <Layout>
-	<div>
-		<p><a href="/create">Create document</a></p>
-		<p><a href="/existing">Existing post</a></p>
-		<p><a href="/existing-author">Existing post (authoring)</a></p>
-		<p><a href="/non-existent">non-existent post</a></p>
-	</div></Layout
->
+	<div slot="aside">
+		<Button on:click={documentFormRef.submitForm} disabled={loading}>Publish</Button>
+	</div>
+	<div class="container">
+		<DocumentForm bind:this={documentFormRef} on:submit={handleSubmit} {loading} />
+	</div>
+</Layout>
+
+<style lang="less">
+	.container {
+		position: relative;
+		height: 100%;
+		min-height: 100vh;
+		display: grid;
+	}
+</style>
